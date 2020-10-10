cbd4086ec719351bb08fdb540f9be10b6f81eafc
Jianshu_Zhao <38149286+jianshu93@users.noreply.github.com>
2020-10-09 23:43:56 -0400

    Update DAS_to_bins.py

--- src/DAS_to_bins.py
+++ src/DAS_to_bins.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 ### This script is used for extract contigs from a large contig file according to the DAS tools output (contig identifier VS bin name)
 ### jianshu.zhao@gatech.edu
-## usage: python DAS_to_bin.py contigs.fa DASTool_scaffolds2bin.txt GWMC_1222_(output prefix)
+## usage: python DAS_to_bin.py contigs.fa DASTool_scaffolds2bin.txt GWMC_1222_(output MAG prefix) ./MAGs (output directory)
 
 import sys
+import os
 file1=str(sys.argv[1])
 file2=str(sys.argv[2])
 prefix=str(sys.argv[3])
+filepath=str(sys.argv[4])
 ## read scaffolds2bin.txt
 rows=""
 dict2 = {}
@@ -23,16 +25,20 @@ with open(file1, "r") as f:
 	for line in f:
 		line = line.strip()
 		if line.startswith('>'):
-			rows = line.split(" ")
 			rows1 = line.strip()
 			table2.append(rows1)
 		else:
 			table2.append(line)
 for i in range(0,len(table2),2):
 	dict1[table2[i]] =  table2[i+1]
-
+### mapping and create new folder/files
 for i in dict1:
 	if str(i)[:str(i).index(" ")] in dict2:
 		file_fh = prefix + str(dict2[str(i)[:str(i).index(" ")]]) + ".fasta"
-		with open(file_fh, "a") as fh:
-			fh.write(str(i) + "\n" + str(dict1[i]) + "\n")
+		if os.path.exists(filepath):
+			with open(os.path.join(filepath,file_fh), "a") as fh:
+				fh.write(str(i) + "\n" + str(dict1[i]) + "\n")
+		else:
+			os.mkdir(filepath)
+			with open(os.path.join(filepath,file_fh, "a")) as fh:
+				fh.write(str(i) + "\n" + str(dict1[i]) + "\n")
